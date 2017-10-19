9d94a7fea55a77b9cb01bf0917a6b00da3e0d579
James Conkling <james.lane.conkling@gmail.com>
2017-10-19 14:30:10 -0400

    add smoke test

---
+++ test/index.js
@@ -0,0 +1,14 @@
+const test = require('tape');
+const concat = require('concat-stream');
+const myriahedron1 = require('../data/myriahedron-1.json');
+const myriahedron2 = require('../data/myriahedron-2.json');
+const subdivideMyriahedron = require('../index.js');
+
+
+test('calling subdivideMyriahedron with myriahedron-1 and depth 2 should output myriahedron-2 [smoke test]', (t) => {
+  t.plan(1);
+
+  subdivideMyriahedron(myriahedron1, 2)
+    .on('error', t.fail)
+    .pipe(concat({ encoding: 'string' }, result => t.deepEqual(JSON.parse(result), myriahedron2)));
+});
